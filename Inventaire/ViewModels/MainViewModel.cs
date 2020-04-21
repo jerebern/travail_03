30dab8153b5a0cc93025c5ecb530dcf084199cfa
Jérémy Bernard <jere.bern@hotmail.com>
2020-04-21 16:29:07 -0400

    Display Invoice command -> video 55:54

--- Inventaire/ViewModels/MainViewModel.cs
+++ Inventaire/ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using BillingManagement.UI.ViewModels.Commands;
+﻿using BillingManagement.Models;
+using BillingManagement.UI.ViewModels.Commands;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -22,10 +23,17 @@ namespace BillingManagement.UI.ViewModels
 		InvoiceViewModel invoiceViewModel;
 
 		public ChangeViewCommand ChangeViewCommand { get; set; }
+		public RelayCommand NewCustomerCommand { get; private set; }
 
+		public RelayCommand DisplayInvoiceCommand { get; private set; }
+
+		public RelayCommand DisplayCustomerCommand { get; private set; }
 		public MainViewModel()
 		{
 			ChangeViewCommand = new ChangeViewCommand(ChangeView);
+			NewCustomerCommand = new RelayCommand(NewCustomer);
+
+			DisplayInvoiceCommand = new RelayCommand(DisplayInvoice);
 
 			customerViewModel = new CustomerViewModel();
 			invoiceViewModel = new InvoiceViewModel(customerViewModel.Customers);
@@ -45,6 +53,23 @@ namespace BillingManagement.UI.ViewModels
 					VM = invoiceViewModel;
 					break;
 			}
+		}
+		
+		private void NewCustomer(Object c)
+		{
+			Customer customer = new Customer();
+			customerViewModel.Customers.Add(customer);
+			customerViewModel.SelectedCustomer = customer;
+
+			VM = customerViewModel;
+		}
+
+		private void DisplayInvoice(object i)
+		{
+			Invoice invoice = i as Invoice;
+
+			invoiceViewModel.SelectedInvoice = invoice;
+			VM = invoiceViewModel;
 		}
 
 	}
